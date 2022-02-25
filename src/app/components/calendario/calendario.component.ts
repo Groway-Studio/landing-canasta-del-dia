1ec7534540fa60a8fa86214ef97e22c0627b7f8d
cnahuina <cnahuina@gmail.com>
2022-02-25 16:31:54 -0500

    download image and active month

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -9,67 +9,79 @@ import {ShareCalendarComponent} from '../shared/share-calendar/share-calendar.co
 })
 export class CalendarioComponent implements OnInit {
 
-  // activeLink = this.links[2];
+  activeLink = 1;
 
   meses = [
     {
+      id : 1,
       month : "Enero",
       images : "assets/febrero-calendar.png",
       img_mobile : "assets/febrero-mobile.png",
       active: "false"
     },
     {
+      id : 2,
       month : "Febrero",
       images : "assets/febrero-calendar.png",
       img_mobile : "assets/febrero-mobile.png",
       active: "true"
     },
     {
+      id : 3,
       month : "Marzo",
       images : "",
       active: "false"
     },
     {
+      id : 4,
       month : "Abril",
       images : "",
       active: "false"
     },
     {
+      id : 5,
       month : "Mayo",
       images : "",
       active: "false"
     },
     {
+      id : 6,
       month : "Junio",
       images : "",
       active: "false"
     },
     {
+      id : 7,
       month : "Julio",
       images : "",
       active: "false"
     },
     {
+      id : 8,
       month : "Agosto",
       images : "",
       active: "false"
     },
     {
+      id : 9,
       month : "Septiembre",
       images : "",
       active: "false"
     },
     {
+      id : 10,
       month : "Octubre",
       images : "",
       active: "false"
     },
     {
+      id : 11,
       month : "Noviembre",
       images : "",
       active: "false"
     },
     {
+      id : 12,
       month : "Diciembre",
       images : "",
       active: "false"
