a326ed7cadd166b5f996b4bcc8c3146644bcf650
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-05-23 15:05:34 -0600

    Season calendar fruits updated

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -11,79 +11,91 @@ export class CalendarioComponent implements OnInit {
 
   activeLink = 1;
 
+  currentMonth = new Date().getMonth()
+
   meses = [
     {
       id : 1,
       month : "Enero",
-      images : "",
-      img_mobile : "",
+      images : "assets/ENERO.svg",
+      img_mobile : "assets/mobile/Enero.svg",
       active: "false"
     },
     {
       id : 2,
       month : "Febrero",
-      images : "assets/febrero-calendar.png",
-      img_mobile : "assets/febrero-mobile.png",
+      images : "assets/FEBRERO.svg",
+      img_mobile : "assets/mobile/Febrero.svg",
       active: "true"
     },
     {
       id : 3,
       month : "Marzo",
-      images : "",
+      images : "assets/MARZO.svg",
+      img_mobile : "assets/mobile/Marzo.svg",
       active: "false"
     },
     {
       id : 4,
       month : "Abril",
-      images : "",
+      images : "assets/ABRIL.svg",
+      img_mobile : "assets/mobile/Abril.svg",
       active: "false"
     },
     {
       id : 5,
       month : "Mayo",
-      images : "",
+      images : "assets/MAYO.svg",
+      img_mobile : "assets/mobile/Mayo.svg",
       active: "false"
     },
     {
       id : 6,
       month : "Junio",
-      images : "",
+      images : "assets/JUNIO.svg",
+      img_mobile : "assets/mobile/Junio.svg",
       active: "false"
     },
     {
       id : 7,
       month : "Julio",
-      images : "",
+      images : "assets/JULIO.svg",
+      img_mobile : "assets/mobile/Julio.svg",
       active: "false"
     },
     {
       id : 8,
       month : "Agosto",
-      images : "",
+      images : "assets/AGOSTO.svg",
+      img_mobile : "assets/mobile/Agosto.svg",
       active: "false"
     },
     {
       id : 9,
       month : "Septiembre",
-      images : "",
+      images : "assets/SEPTIEMBRE.svg",
+      img_mobile : "assets/mobile/Septiembre.svg",
       active: "false"
     },
     {
       id : 10,
       month : "Octubre",
-      images : "",
+      images : "assets/OCTUBRE.svg",
+      img_mobile : "assets/mobile/Octubre.svg",
       active: "false"
     },
     {
       id : 11,
       month : "Noviembre",
-      images : "",
+      images : "assets/NOVIEMBRE.svg",
+      img_mobile : "assets/mobile/Noviembre.svg",
       active: "false"
     },
     {
       id : 12,
       month : "Diciembre",
-      images : "",
+      images : "assets/DICIEMBRE.svg",
+      img_mobile : "assets/mobile/Diciembre.svg",
       active: "false"
     }
 
