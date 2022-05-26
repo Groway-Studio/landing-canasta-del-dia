f87b659c4cc8b140e3a43fd2a8cf90d0d9f4a0c8
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-05-26 16:04:26 -0600

    Images and files to download updated

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,121 +1,121 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {ShareCalendarComponent} from '../shared/share-calendar/share-calendar.component'
+import { MatDialog } from '@angular/material/dialog';
+import { ShareCalendarComponent } from '../shared/share-calendar/share-calendar.component';
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
-  styleUrls: ['./calendario.component.scss']
+  styleUrls: ['./calendario.component.scss'],
 })
 export class CalendarioComponent implements OnInit {
-
   activeLink = 1;
 
-  currentMonth = new Date().getMonth()
+  currentMonth = new Date().getMonth();
 
   months = [
     {
-      id : 1,
-      month : "Enero",
-      images : "assets/ENERO.svg",
-      img_mobile : "assets/mobile/Enero.svg",
-      active: "false"
+      id: 1,
+      month: 'Enero',
+      images: 'assets/desktop/ENERO.svg',
+      img_mobile: 'assets/mobile/Enero.svg',
+      active: 'false',
     },
     {
-      id : 2,
-      month : "Febrero",
-      images : "assets/FEBRERO.svg",
-      img_mobile : "assets/mobile/Febrero.svg",
-      active: "true"
+      id: 2,
+      month: 'Febrero',
+      images: 'assets/desktop/FEBRERO.svg',
+      img_mobile: 'assets/mobile/Febrero.svg',
+      active: 'true',
     },
     {
-      id : 3,
-      month : "Marzo",
-      images : "assets/MARZO.svg",
-      img_mobile : "assets/mobile/Marzo.svg",
-      active: "false"
+      id: 3,
+      month: 'Marzo',
+      images: 'assets/desktop/MARZO.svg',
+      img_mobile: 'assets/mobile/Marzo.svg',
+      active: 'false',
     },
     {
-      id : 4,
-      month : "Abril",
-      images : "assets/ABRIL.svg",
-      img_mobile : "assets/mobile/Abril.svg",
-      active: "false"
+      id: 4,
+      month: 'Abril',
+      images: 'assets/desktop/ABRIL.svg',
+      img_mobile: 'assets/mobile/Abril.svg',
+      active: 'false',
     },
     {
-      id : 5,
-      month : "Mayo",
-      images : "assets/MAYO.svg",
-      img_mobile : "assets/mobile/Mayo.svg",
-      active: "false"
+      id: 5,
+      month: 'Mayo',
+      images: 'assets/desktop/MAYO.svg',
+      img_mobile: 'assets/mobile/Mayo.svg',
+      active: 'false',
     },
     {
-      id : 6,
-      month : "Junio",
-      images : "assets/JUNIO.svg",
-      img_mobile : "assets/mobile/Junio.svg",
-      active: "false"
+      id: 6,
+      month: 'Junio',
+      images: 'assets/desktop/JUNIO.svg',
+      img_mobile: 'assets/mobile/Junio.svg',
+      active: 'false',
     },
     {
-      id : 7,
-      month : "Julio",
-      images : "assets/JULIO.svg",
-      img_mobile : "assets/mobile/Julio.svg",
-      active: "false"
+      id: 7,
+      month: 'Julio',
+      images: 'assets/desktop/JULIO.svg',
+      img_mobile: 'assets/mobile/Julio.svg',
+      active: 'false',
     },
     {
-      id : 8,
-      month : "Agosto",
-      images : "assets/AGOSTO.svg",
-      img_mobile : "assets/mobile/Agosto.svg",
-      active: "false"
+      id: 8,
+      month: 'Agosto',
+      images: 'assets/desktop/AGOSTO.svg',
+      img_mobile: 'assets/mobile/Agosto.svg',
+      active: 'false',
     },
     {
-      id : 9,
-      month : "Septiembre",
-      images : "assets/SEPTIEMBRE.svg",
-      img_mobile : "assets/mobile/Septiembre.svg",
-      active: "false"
+      id: 9,
+      month: 'Septiembre',
+      images: 'assets/desktop/SEPTIEMBRE.svg',
+      img_mobile: 'assets/mobile/Septiembre.svg',
+      active: 'false',
     },
     {
-      id : 10,
-      month : "Octubre",
-      images : "assets/OCTUBRE.svg",
-      img_mobile : "assets/mobile/Octubre.svg",
-      active: "false"
+      id: 10,
+      month: 'Octubre',
+      images: 'assets/desktop/OCTUBRE.svg',
+      img_mobile: 'assets/mobile/Octubre.svg',
+      active: 'false',
     },
     {
-      id : 11,
-      month : "Noviembre",
-      images : "assets/NOVIEMBRE.svg",
-      img_mobile : "assets/mobile/Noviembre.svg",
-      active: "false"
+      id: 11,
+      month: 'Noviembre',
+      images: 'assets/desktop/NOVIEMBRE.svg',
+      img_mobile: 'assets/mobile/Noviembre.svg',
+      active: 'false',
     },
     {
-      id : 12,
-      month : "Diciembre",
-      images : "assets/DICIEMBRE.svg",
-      img_mobile : "assets/mobile/Diciembre.svg",
-      active: "false"
-    }
+      id: 12,
+      month: 'Diciembre',
+      images: 'assets/desktop/DICIEMBRE.svg',
+      img_mobile: 'assets/mobile/Diciembre.svg',
+      active: 'false',
+    },
+  ];
 
-  ]
-
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {}
   openDialog(month: number) {
     const currentMonth = this.months[month].images.split('/');
-    const getMonthImageUrl = currentMonth[0] + '/share-images/' + currentMonth[1].split('.')[0] + '.png';
+    const getMonthImageUrl = `${currentMonth[0]}/share-files/${
+      currentMonth[2].split('.')[0]
+    }.pdf`;
 
     const dialogRef = this.dialog.open(ShareCalendarComponent, {
-      data: {getCurrentMonth: getMonthImageUrl}
+      data: { getCurrentMonth: getMonthImageUrl },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    console.log(currentMonth, getMonthImageUrl);
+
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/shared/share-calendar/share-calendar.component.ts
+++ src/app/components/shared/share-calendar/share-calendar.component.ts
@@ -1,28 +1,34 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-share-calendar',
   templateUrl: './share-calendar.component.html',
-  styleUrls: ['./share-calendar.component.scss']
+  styleUrls: ['./share-calendar.component.scss'],
 })
 export class ShareCalendarComponent implements OnInit {
-  month = this.data.getCurrentMonth.split('/')[2];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {getCurrentMonth: string}) {}
-  downloadImg(){
+  month = this.data.getCurrentMonth.split('/')[2].split('.')[0] + '.pdf';
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { getCurrentMonth: string }
+  ) {}
+  downloadImg() {
     // saveAs(url, name+'.png');
   }
 
-  shareImage () {
-    return "https://lacanastadehoy.com/" + this.data.getCurrentMonth;
+  shareImage() {
+    const pathCurrentMonth = `${this.data.getCurrentMonth.slice(0, -3)}pdf`;
+
+    console.log(pathCurrentMonth);
+    console.log(this.month);
+
+    return `https://lacanastadehoy.com//${pathCurrentMonth}`;
   }
 
   getDescription() {
     const month = this.month.split('.')[0];
-    return "La Canasta de Hoy - Calendario de Temporada " + month;
+    return 'La Canasta de Hoy - Calendario de Temporada ' + month;
   }
 
   ngOnInit(): void {}
-
 }
